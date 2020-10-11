cba559607d2033755a410f56e6d30e3569faf605
MHD22 <mhd.sad22@gmail.com>
2020-10-11 16:28:59 +0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var Seattle ={
+  minCoust:23,
+  maxCoust:65,
+  avgCookies:3.6,
+  cookiesPerHour:[],//how cookies saled per hour( avg X random Costumer) from 6AM to 8 PM
+  total:0 ,// hourly totals  cookies 
+
+  render: function(){}, // render the hourly cookies saled with total as un ordered list
+  
+
+}
